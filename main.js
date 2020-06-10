0484ef7a55b00eb06f6943c32787947f543744d2
Luca Santini <lucasantinidev@gmail.com>
2020-06-10 16:08:42 +0200

    richiamo API sales

--- main.js
+++ main.js
@@ -1,37 +1,26 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+$(document).ready(function() {
+  $.ajax({
+      'url': '157.230.17.132:4027/sales',
+      'method': 'GET',
+      'success': function(data) {
+          console.log(data);
+        },
+        'error': function() {
+          console.log('errore');
+      }
+  })
+})
+// "sales": [
+//     {
+//     "id": 1,
+//     "salesman": "Marco",
+//     "amount": 9000,
+//     "date": "12/02/2017"
+//     },
+//     {
+//     "id": 2,
+//     "salesman" : "Giuseppe",
+//     "amount": 1000,
+//     "date": "12/04/2017"
+//      }
+//   ]
